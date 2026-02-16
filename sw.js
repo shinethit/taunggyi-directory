8e51974a089ff9147cc492128481b249a44a629c
shinethit <khunthanshwe@gmail.com>
2026-02-17 02:01:31 +0630

    sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'cherry-v5';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/CDST_Logo.png',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
