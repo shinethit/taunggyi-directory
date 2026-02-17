69fc94b3926ee9aec8255d6ad20af827ab429d0b
shinethit <khunthanshwe@gmail.com>
2026-02-17 11:54:48 +0630

    sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,6 @@
-// SERVICE WORKER SELF-DESTRUCT MODE (Version 34)
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
-
+// CACHE CLEANER V35
+self.addEventListener('install', (e) => self.skipWaiting());
 self.addEventListener('activate', (e) => {
-  // 1. Delete ALL Caches
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-  );
-  // 2. Unregister Self (Stop running forever)
-  self.registration.unregister()
-    .then(() => console.log('Service Worker: Unregistered'))
-    .catch(err => console.log('Service Worker Error:', err));
+    e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+    self.registration.unregister();
 });
