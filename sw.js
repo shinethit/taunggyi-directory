ffb24be6dd8c4c6cce3eabf459b7f78f1f3fcc76
shinethit <khunthanshwe@gmail.com>
2026-02-17 11:39:54 +0630

    sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
-// SERVICE WORKER KILLER (VERSION 33)
+// SERVICE WORKER SELF-DESTRUCT MODE (Version 34)
 self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  // Cache အားလုံးကို ဖျက်မည်
+  // 1. Delete ALL Caches
   e.waitUntil(
     caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
   );
-  // Service Worker ကို ဖြုတ်မည်
-  self.registration.unregister();
+  // 2. Unregister Self (Stop running forever)
+  self.registration.unregister()
+    .then(() => console.log('Service Worker: Unregistered'))
+    .catch(err => console.log('Service Worker Error:', err));
 });
