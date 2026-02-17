768c911219efb43706616cd83df45fe5828ca002
shinethit <khunthanshwe@gmail.com>
2026-02-17 11:57:28 +0630

    sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,11 @@
-// CACHE CLEANER V35
-self.addEventListener('install', (e) => self.skipWaiting());
+// SERVICE WORKER KILLER (Version 36)
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
 self.addEventListener('activate', (e) => {
-    e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
-    self.registration.unregister();
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
+  self.registration.unregister();
 });
