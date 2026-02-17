146b83f7c76aa07fa5a514818386d101c83ce3ef
shinethit <khunthanshwe@gmail.com>
2026-02-17 13:27:51 +0630

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// SERVICE WORKER KILLER (Version 42)
+// SERVICE WORKER KILLER (Version 43)
 self.addEventListener('install', (e) => self.skipWaiting());
 self.addEventListener('activate', (e) => {
     e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
